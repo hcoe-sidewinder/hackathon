3f4c106dfde2271571aa017ba36a64130da2b3f0
amritchapai <chapaiamrit@gmail.com>
2025-02-09 23:37:14 +0545

    lucide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import ProfilePage from "./Pages/ProfilePage";
 import { sampleUser } from "./assets/sampleData";
+import SignupPage from "./Pages/SignupPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignupPage/>}></Route>
+
         <Route
           path="/profile"
           element={<ProfilePage user={sampleUser} />}
