8c73a2c2ec6e4733f96a495627a8c64c93da7047
FosRex <gahireanshu1011@gmail.com>
2025-02-09 19:44:20 +0545

    Added users registration endpoint

--- backend/routes/users/create.js
+++ backend/routes/users/create.js
@@ -1,20 +1,31 @@
+import bcrypt from "bcryptjs";
+
 import User from "../../models/user/user.schema.js";
 import validateEmail from "../../middleware/email.validate.js";
 
 import HTTP_STATUS_CODE from "../../utils/status.codes.js";
 import { SALT } from "../../utils/constants.js";
+import addBank from "../bank/create.js";
 
 const addUser = async (req, res) => {
   try {
-    let { name, email, password } = req.body;
+    const userDetails = req.body;
 
-    if (!validateEmail(email)) {
+    if (!validateEmail(userDetails.email)) {
       return res
         .status(HTTP_STATUS_CODE.INVALID)
-        .json({ message: "Not a valid email", success: false });
+        .json({ message: "Not a valid userDetails.email", success: false });
     }
 
-    const emailAlreadyExists = await User.findOne({ email: email });
+    const panAlreadyExists = await User.findOne({ panNo: userDetails.panNo });
+
+    if (panAlreadyExists) {
+      return res
+        .status(HTTP_STATUS_CODE.INVALID)
+        .json({ message: "Duplicate Pan no", success: false });
+    }
+
+    const emailAlreadyExists = await User.findOne({ email: userDetails.email });
 
     if (emailAlreadyExists) {
       return res
@@ -22,16 +33,35 @@ const addUser = async (req, res) => {
         .json({ message: "Email already exists", success: false });
     }
 
-    password = await bcrypt.hash(password, SALT);
+    const bank = await addBank({
+      bankName: userDetails.bankName,
+      accNo: userDetails.accNo,
+      accName: userDetails.accName,
+    });
+
+    console.log(bank);
+
+    if (!bank) {
+      return res
+        .status(HTTP_STATUS_CODE.INVALID)
+        .json({ message: "Bank details not provided", success: false });
+    }
+
+    userDetails.password = await bcrypt.hash(userDetails.password, SALT);
 
     try {
       await User.create({
-        name: name,
-        email: email,
-        password: password,
+        panNo: userDetails.panNo,
+        name: userDetails.name,
+        email: userDetails.email,
+        password: userDetails.password,
+        phNo: userDetails.phNo,
+        panImg: userDetails.panImg,
+        profilePic: userDetails.profilePic,
+        bankId: bank.id,
       });
 
-      res.status(HTTP_STATUS_CODE.OK).json({
+      return res.status(HTTP_STATUS_CODE.OK).json({
         message: "User created successfully",
         success: true,
       });
