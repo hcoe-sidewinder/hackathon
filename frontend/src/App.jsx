cc871c995056fffd4d996243964bf104046349a5
preykriti <prakriteeshrestha2@gmail.com>
2025-02-09 21:38:04 +0545

    profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
+import { sampleUser } from "./assets/sampleData";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route
+          path="/profile"
+          element={<ProfilePage user={sampleUser} />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
