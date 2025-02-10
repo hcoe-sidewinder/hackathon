a0aab1b565adaef1b5bdf53d78ff126a330fbb8e
amritchapai <chapaiamrit@gmail.com>
2025-02-10 11:33:54 +0545

    started with state

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "sonner";
+import { AuthProvider } from "./context/authContext.js";
+import { TradeProvider } from "./context/tradeContext.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Toaster/>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <TradeProvider>
+        <App />
+        <Toaster />
+      </TradeProvider>
+    </AuthProvider>
+  </StrictMode>
+);
