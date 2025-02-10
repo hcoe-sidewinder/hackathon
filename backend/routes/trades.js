c1f5eb13d86bef9af20741f36a768f1326237509
FosRex <gahireanshu1011@gmail.com>
2025-02-10 09:04:33 +0545

    Added complete phase endpoint

--- backend/routes/trades.js
+++ backend/routes/trades.js
@@ -1,21 +1,31 @@
 import express from "express";
 
 import validateUser from "../middleware/user.validate.js";
+
 import addTrade from "./trade/create.js";
+import { getTrades, getTradeById } from "./trade/read.js";
+import completePhase from "./trade/phase/update.js";
 
 const router = express.Router();
 
 //get trades
-//router.get("/", getBanks);
+router.get("/", getTrades);
 
 //get trade by id
-//router.get("/:tradeId(\\w+)", getUsersById);
+router.get("/:tradeId(\\w+)", getTradeById);
 
 // add trade
 router.post("/add", validateUser, addTrade);
 
 // edit trade by id
 //router.put("/edit/:userId(\\d+)", editUser);
+
+// complete phase by trade id
+router.put(
+  "/:tradeId(\\w+)/completePhase/:phase(\\d+)",
+  validateUser,
+  completePhase,
+);
 
 // delete trade by id
 //router.delete("/delete/:userId(\\d+)", deleteUser);
